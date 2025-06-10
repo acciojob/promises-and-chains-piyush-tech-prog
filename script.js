d1349ecd0d56bc282853fa14deb5fb862a9ff46a
Piyush Sharma <piyushopsaws@gmail.com>
2025-06-10 19:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const checkage = ()=>{
+            const age = document.getElementById("age").value
+            const name = document.getElementById("name").value
+
+            if(!age || !name){
+                alert("Please Enter valid Details")
+            }
+            const prom = new Promise((res,rej)=>{
+                if(age>18){
+                   setTimeout(()=>{res(alert("Welcome . You can Vote")); location.reload()},4000)
+                }else{
+                    setTimeout(()=>{rej(alert("Oh sorry . You aren't old enough")); location.reload()},4000)
+                }
+               
+            })
+        }
